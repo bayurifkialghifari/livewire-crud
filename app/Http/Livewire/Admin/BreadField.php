72000736921c2711f4f42d72b5a6c13429510c48
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2023-02-09 19:04:12 +0700

    add crud bread field

--- app/Http/Livewire/Admin/BreadField.php
+++ app/Http/Livewire/Admin/BreadField.php
@@ -39,6 +39,8 @@ class BreadField extends Component
         $list_db_table,
         $list_origin_field = [],
         $list_foreign_field = [],
+        $is_foreign = 0,
+        $crud_new = [],
         $crud_value = [
             'bread_id' => 0,
             'type' => [],
@@ -66,15 +68,23 @@ class BreadField extends Component
         $this->bread_id = $id;
 
         // Bread detail
-        $bread = Bread::find($id);
-        $bread_field = BreadFields::where('bread_id', $id)->get();
+        $this->getBreadDetail();
+    }
 
-        $this->crud_value['bread_id'] = $id;
+    public function getBreadDetail()
+    {
+        $bread = Bread::find($this->bread_id);
+        $bread_field = BreadFields::where('bread_id', $this->bread_id)->get();
+
+        $this->crud_value['bread_id'] = $this->bread_id;
         $this->primary_table = $bread->table_name;
         $this->primary_key = $bread->primary_key;
 
         // Initiate crud value
         foreach ($bread_field as $bf) {
+            $this->crud_value['foreign_table'][$bf->id] = $bf->foreign_table;
+            $this->crud_value['foreign_key'][$bf->id] = $bf->foreign_key;
+            $this->crud_value['foreign_field'][$bf->id] = $bf->foreign_field;
             $this->crud_value['type'][$bf->id] = $bf->type;
             $this->crud_value['display_name'][$bf->id] = $bf->display_name;
             $this->crud_value['placeholder'][$bf->id] = $bf->placeholder;
@@ -111,7 +121,8 @@ class BreadField extends Component
 
         // Get list db table
         $this->list_db_table = DB::select('SHOW TABLES');
-        $this->list_origin_field = DB::select('SHOW COLUMNS FROM '.$this->primary_table);
+        $this->list_origin_field = DB::select('SHOW COLUMNS FROM ' . $bread->table_name);
+        array_push($this->exluded_table, $bread->table_name);
 
         return view('livewire.admin.bread-field', compact('data', 'bread', 'title'))->layoutData(compact('active_menu'));
     }
@@ -129,5 +140,82 @@ class BreadField extends Component
     public function confirmDelete($id)
     {
         $this->emit('confirm', $id);
+    }
+
+    // Get detail table
+    public function getDetailTable()
+    {
+        $this->list_foreign_field = DB::select('SHOW COLUMNS FROM ' . $this->crud_new['foreign_table']);
+    }
+
+    // Update is foreign
+    public function updateForeign()
+    {
+        $this->crud_new['foreign_table'] = null;
+        $this->crud_new['foreign_key'] = null;
+        $this->crud_new['field'] = null;
+        $this->crud_new['foreign_field'] = null;
+    }
+
+    // isCreate new field
+    public function isCreate()
+    {
+        $this->crud_new = [
+            'bread_id' => $this->bread_id,
+            'foreign_table' => null,
+            'foreign_key' => null,
+            'foreign_field' => null,
+            'field' => null,
+            'type' => 'text',
+            'is_required' => 0,
+            'is_readonly' => 0,
+            'is_searchable' => 1,
+            'is_browse' => 1,
+            'is_edit' => 1,
+            'is_add' => 1,
+        ];
+    }
+
+    // Create new field
+    public function createNewField()
+    {
+        // Create
+        $exe = BreadFields::create($this->crud_new);
+
+        $this->getBreadDetail();
+        $this->emit('alert', 'Data created');
+        $this->emit('closeModal');
+    }
+
+    // Save
+    public function save()
+    {
+        // Get key
+        foreach($this->crud_value['type'] as $k => $v) {
+            // Update
+            $exe = BreadFields::find($k)->update([
+                'foreign_table' => $this->crud_value['foreign_table'][$k],
+                'foreign_key' => $this->crud_value['foreign_key'][$k],
+                'foreign_field' => $this->crud_value['foreign_field'][$k],
+                'type' => $this->crud_value['type'][$k],
+                'display_name' => $this->crud_value['display_name'][$k],
+                'placeholder' => $this->crud_value['placeholder'][$k],
+                'class_alt' => $this->crud_value['class_alt'][$k],
+                'default_value' => $this->crud_value['default_value'][$k],
+                'id_alt' => $this->crud_value['id_alt'][$k],
+                'is_required' => $this->crud_value['is_required'][$k],
+                'is_readonly' => $this->crud_value['is_readonly'][$k],
+                'is_searchable' => $this->crud_value['is_searchable'][$k],
+                'is_browse' => $this->crud_value['is_browse'][$k],
+                'is_edit' => $this->crud_value['is_edit'][$k],
+                'is_add' => $this->crud_value['is_add'][$k],
+                'source' => $this->crud_value['source'][$k],
+                'source_id' => $this->crud_value['source_id'][$k],
+                'source_value' => $this->crud_value['source_value'][$k],
+                'file_accept' => $this->crud_value['file_accept'][$k],
+                'description' => $this->crud_value['description'][$k],
+                'description_class' => $this->crud_value['description_class'][$k],
+            ]);
+        }
     }
 }
--- resources/views/livewire/admin/bread-field.blade.php
+++ resources/views/livewire/admin/bread-field.blade.php
@@ -21,7 +21,7 @@
                 <div class="card-header">
                     <div class="row">
                         <div class="col-lg-12 col-md-12">
-                            <button class="btn btn-warning float-end mt-3">
+                            <button class="btn btn-warning float-end mt-3" wire:click="isCreate" data-bs-toggle="modal" data-bs-target="#modal-crud">
                                 <i class="bi bi-plus"></i>
                                 Add new field
                             </button>
@@ -48,7 +48,7 @@
                                     @foreach ($data as $d)
                                         <tr>
                                             <td>{{ $d->foreign_table ?? $d->bread  }}</td>
-                                            <td>{{ $d->field }}</td>
+                                            <td>{{ $d->field ?? $d->bread_fields }}</td>
                                             {{-- Visibility --}}
                                             <td>
                                                 <input type="checkbox" wire:model="crud_value.is_searchable.{{ $d->id }}" class="form-check-input" />Browse<br>
@@ -60,9 +60,9 @@
                                             <td>
                                                 <input type="checkbox" wire:model="crud_value.is_readonly.{{ $d->id }}" class="form-check-input" />Readonly<br>
                                                 <input type="checkbox" wire:model="crud_value.is_required.{{ $d->id }}" class="form-check-input" />Required<br>
-                                                <button class="btn btn-primary btn-sm">
+                                                {{-- <button class="btn btn-primary btn-sm">
                                                     <i class="bi bi-plus"></i> Aditional Rules
-                                                </button>
+                                                </button> --}}
                                             </td>
                                             {{-- Input type --}}
                                             <td>
@@ -119,15 +119,18 @@
                                                 <input type="text" placeholder="Description class" wire:model="crud_value.description_class.{{ $d->id }}">
                                                 <br>
                                                 <br>
-                                                <button class="btn btn-primary btn-sm">
+                                                {{-- <button class="btn btn-primary btn-sm">
                                                     <i class="bi bi-plus"></i> Aditional field setting
+                                                </button> --}}
+                                                <button class="btn btn-danger btn-sm" wire:click="confirmDelete('{{ $d->id }}')">
+                                                    <i class="bi bi-trash"></i> Delete
                                                 </button>
                                             </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
                             </table>
-                            <button class="btn btn-success float-end mb-5">
+                            <button class="btn btn-success float-end mb-5" wire:click="save">
                                 <i class="bi bi-save"></i>
                                 Save
                             </button>
@@ -135,6 +138,100 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div wire:ignore.self class="modal fade" id="modal-crud" tabindex="-1" aria-labelledby="modal-crud-title"
+    aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <form wire:submit.prevent="createNewField">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal-crud-title">
+                            Create New Field {{ ucwords($bread->table_name) }}
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" wire:model="crud_new.bread_id">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label>Is Foreign</label>
+                                <select wire:model="is_foreign" wire:change="updateForeign" class="form-control">
+                                    <option value="0">No</option>
+                                    <option value="1">Yes</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="{{ $is_foreign ? 'd-block' : 'd-none'}}">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Foreign table</label>
+                                    <select wire:model="crud_new.foreign_table" wire:change="getDetailTable()" class="form-control">
+                                        <option value="">--Select table--</option>
+                                        @foreach ($list_db_table as $origin)
+                                            @php
+                                                $val_origin = json_decode(json_encode($origin), true);
+                                            @endphp
+                                            @if(!in_array($val_origin['Tables_in_' . env('DB_DATABASE')], $exluded_table))
+                                                <option value="{{ $val_origin['Tables_in_' . env('DB_DATABASE')] }}">{{ $val_origin['Tables_in_' . env('DB_DATABASE')] }}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Foreign Key</label>
+                                    <select wire:model.lazy="crud_new.foreign_key" class="form-control">
+                                        <option value="">--Select field--</option>
+                                        @foreach ($list_foreign_field as $ff)
+                                            @php
+                                                $v_ff = json_decode(json_encode($ff), true);
+                                            @endphp
+                                            <option value="{{ $v_ff->Field ?? $v_ff['Field'] }}">{{ $v_ff->Field ?? $v_ff['Field'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Foreign Feild <b>(Value location save)</b></label>
+                                    <select wire:model.lazy="crud_new.foreign_field" class="form-control">
+                                        <option value="">--Select field--</option>
+                                        @foreach ($list_foreign_field as $ff)
+                                            @php
+                                                $v_ff = json_decode(json_encode($ff), true);
+                                            @endphp
+                                            <option value="{{ $v_ff->Field ?? $v_ff['Field'] }}">{{ $v_ff->Field ?? $v_ff['Field'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="{{ $is_foreign ? 'd-none' : 'd-block'}}">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Feild</b></label>
+                                    <select wire:model.lazy="crud_new.field" class="form-control">
+                                        <option value="">--Select field--</option>
+                                        @foreach ($list_origin_field as $origin)
+                                            @php
+                                                $v_origin = json_decode(json_encode($origin), true);
+                                            @endphp
+                                            <option value="{{ $v_origin->Field ?? $v_origin['Field'] }}">{{ $v_origin->Field ?? $v_origin['Field'] }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </section>
